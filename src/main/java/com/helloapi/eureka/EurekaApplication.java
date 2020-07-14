a3441ca064b0816209e4be9e83ecf5af3b3c70f5
gdmail91 <gdmail91@gmail.com>
2020-07-14 11:34:09 +0900

    init

---
+++ src/main/java/com/helloapi/eureka/EurekaApplication.java
@@ -0,0 +1,17 @@
+package com.helloapi.eureka;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+@EnableEurekaServer
+@SpringBootApplication
+// @EnableAutoConfiguration
+public class EurekaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EurekaApplication.class, args);
+	}
+
+}
